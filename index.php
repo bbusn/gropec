58187e3bae9627e2de1b769914ea6c598c72f416
Benoit Busnardo <benoit.busnardo@gmail.com>
2024-03-05 14:37:22 +0100

    version from 0.9.0 to 1.0.0

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ define('ROOT', '/gropec/');
 // define('ROOT', '/');
 define('ROOT_SEGMENTS', '4');
 // define('ROOT_SEGMENTS', '3');
-define('VERSION', '0.9.0');
+define('VERSION', '1.0.0');
 define('AUTHOR_URL', 'https://benoitbusnardo.fr');
 define('V_QUERY', '?v='.VERSION);
 /*____________ SESSION ____________*/
